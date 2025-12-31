3c6733a72246ffcbb93e24aeaee95250b634f31b
AxelW <axelwestman@outlook.com>
2025-12-31 20:11:25 -0300

    Agrego comentarios para guiarme

--- src/main/java/com/axl/backend/model/Role.java
+++ src/main/java/com/axl/backend/model/Role.java
@@ -2,26 +2,26 @@ package com.axl.backend.model;
 import jakarta.persistence.*;
 
 
-@Entity
-@Table(name = "roles")
+@Entity //le decimos a JPA que esto es una entidad
+@Table(name = "roles") //Indica el nombre de la tabla en la BD
 public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id//con esto indicamos que será la PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //indicamos que es autoincremental y el valor lo genera la BD
+    private Long id; //Usamos siempre Long, no int
 
-    @Column(nullable = false, unique = true)
-    private String rol;
+    @Column(nullable = false, unique = true) //indicamos que esta columan no puede ser nula, y es unica
+    private String rol; //en la bd será varchar, aca es string
 
-    // 🔹 Constructor vacío (OBLIGATORIO para JPA)
+    // 🔹 Constructor vacío (OBLIGATORIO para JPA), Hibernate crea obj por reflexion, sin esto no arranca
     public Role() {
     }
 
-    // 🔹 Constructor útil
     public Role(String rol) {
         this.rol = rol;
-    }
+    } //constructor útil (opcional pero recomendado)
+    //esto sirve para crear objectos fácil en código con 'new Role("ADMIN")'
 
-    // 🔹 Getters & Setters
+    // 🔹 Getters & Setters. Spring, Jackson y Hibernate los usan, sirven para serializar JSON, para escribir y leer datos
     public Long getId() {
         return id;
     }
