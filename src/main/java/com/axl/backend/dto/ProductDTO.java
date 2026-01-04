9b767ab8ac66fd2cf861c0ac86167fa464b22632
AxelW <axelwestman@outlook.com>
2026-01-03 23:00:02 -0300

    Creando el DTO de "productos"

--- src/main/java/com/axl/backend/dto/ProductDTO.java
+++ src/main/java/com/axl/backend/dto/ProductDTO.java
@@ -1,4 +1,49 @@
 package com.axl.backend.dto;
+import java.math.BigDecimal;
 
 public class ProductDTO {
+
+private String name_product;
+private String description;
+private BigDecimal price;
+private Long category_id;
+
+public ProductDTO(String name_product, String description, BigDecimal price, Long category_id){
+    this.name_product = name_product;
+    this.description = description;
+    this.price = price;
+    this.category_id = category_id;
+}
+
+    public String getName_product() {
+        return name_product;
+    }
+
+    public void setName_product(String name_product) {
+        this.name_product = name_product;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
 }
